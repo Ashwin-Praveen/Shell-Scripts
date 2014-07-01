844df4ec493056ec596d1a02316d8d31df3b3c83
Ashwin-Praveen <ashwinpraveen1@gmail.com>
2014-07-02 00:07:48 +0530

    comments

--- runC.sh
+++ runC.sh
@@ -1,13 +1,18 @@
 #!/bin/bash 
+#Program that helps the user edit, compile and run their code easily 
 
 clear
+#Clears the screen so that everything looks clean
 
 file=`ls -t *.c | head -n 1`
+#Svaing last used C file 
 
 echo "\n\nDo you want to \n1.Edit Your File\n2.Compile code\n3.Run Executable\n"
 echo "Choice: \c" > /dev/tty
+#\c to keep the cursor there
 
 read choice
+#Reads input
 
 case $choice in
 	
@@ -15,6 +20,7 @@ case $choice in
 		read bin
 		echo $bin > binsave 
 
+#We are saving the exe file in binsave file so that we can access it later for the run case
 		
 		echo " "
 		gcc "$file" -o $bin && echo "File compiled succesfully." ;;
@@ -22,8 +28,10 @@ case $choice in
 	1)	nano $file ;;
 
 	3)	./`cat binsave` || echo "\nPlease Compile your program." ;;
+#this reads the exe file stored in file and appends it with ./ to give proper output
 
 	*)	echo "Enter the one of the options mentioned above." ;; 
+#Any other choice other than 1,2,3 is given this output
 
 esac
 #end of case
